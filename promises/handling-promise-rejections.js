76f31435d3967387acc4e2913cc26fb64529d18e
Raj Jain <raj.jain@credera.co.uk>
2023-03-27 15:38:43 +0100

    handling promise rejections

--- promises/handling-promise-rejections.js
+++ promises/handling-promise-rejections.js
@@ -11,18 +11,37 @@ function calculateSquare(number) {
   return promise;
 }
 
+// Example 1
+
+// calculateSquare(1)
+//   .then((value) => {
+//     console.log(value);
+//     return calculateSquare(2);
+//   })
+//   .then(
+//     (value) => {
+//       throw new Error("some random error");
+//     },
+//     (reason) => {
+//       console.log("error happened: " + reason.message);
+//     }
+//   )
+//   .catch((reason) => {
+//     console.log(reason.message);
+//   });
+
+// Example 2
+
 calculateSquare(1)
   .then((value) => {
     console.log(value);
     return calculateSquare(2);
   })
-  .then(
-    (value) => {
-      throw new Error("some random error");
-
-      //   console.log(value);
-    },
-    (reason) => {
-      console.log("error happened: " + reason.message);
-    }
-  );
+  .then(() => {
+    return new Promise(function (resolve, reject) {
+      reject(new Error("Something went wrong"));
+    });
+  })
+  .catch((reason) => {
+    console.log("Error happened: " + reason.message);
+  });
