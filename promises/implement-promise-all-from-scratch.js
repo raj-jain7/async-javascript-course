caaad84b3513472bd9f037ec376068c38f91a197
Raj Jain <raj.jain@credera.co.uk>
2023-03-27 19:12:40 +0100

    implementing promise all

--- promises/implement-promise-all-from-scratch.js
+++ promises/implement-promise-all-from-scratch.js
@@ -6,13 +6,19 @@ function askFirstDealer() {
 
 function askSecondDealer() {
   return new Promise(function (resolve, reject) {
-    setTimeout(() => resolve(12000), 5000);
+    setTimeout(() => resolve(12000), 12000);
   });
 }
 
 function askThirdDealer() {
   return new Promise(function (resolve, reject) {
     setTimeout(() => resolve(10000), 8000);
+  });
+}
+
+function askThirdDealerFails() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => reject("No reason provided"), 1000);
   });
 }
 
@@ -27,19 +33,36 @@ const promiseAll = function (arrayOfPromises) {
 
   return new Promise((resolve, reject) => {
     arrayOfPromises.forEach((promise, index) => {
-      promise.then((result) => {
-        settledPromisesCount += 1;
-        promiseValues[index] = result;
-        if (settledPromisesCount === arrayOfPromises.length) {
-          resolve(promiseValues);
-        }
-      });
+      promise
+        .then((result) => {
+          settledPromisesCount += 1;
+          promiseValues[index] = result;
+          if (settledPromisesCount === arrayOfPromises.length) {
+            resolve(promiseValues);
+          }
+        })
+        // If one promise fails, need to reject all of them
+        .catch((reason) => {
+          reject(reason);
+        });
     });
   });
 };
 
-promiseAll([askFirstDealer(), askSecondDealer(), askThirdDealer()]).then(
-  (values) => {
+// Example 1 - Working
+
+// promiseAll([askFirstDealer(), askSecondDealer(), askThirdDealer()]).then(
+//   (values) => {
+//     console.log(values);
+//   }
+// );
+
+// Example 2 - Not working
+
+promiseAll([askFirstDealer(), askSecondDealer(), askThirdDealerFails()])
+  .then((values) => {
     console.log(values);
-  }
-);
+  })
+  .catch((reason) => {
+    console.log(reason);
+  });
