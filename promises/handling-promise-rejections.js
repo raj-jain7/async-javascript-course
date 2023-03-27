1e1d80e958836264b0dfb5e4850e26529b11a56c
Raj Jain <raj.jain@credera.co.uk>
2023-03-27 15:27:40 +0100

    wip

--- promises/handling-promise-rejections.js
+++ promises/handling-promise-rejections.js
@@ -1,1 +1,28 @@
+function calculateSquare(number) {
+  const promise = new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      if (typeof number !== "number") {
+        return reject(new Error("Argument of type number is expected"));
+      }
+      const result = number * number;
+      resolve(result);
+    }, 1000);
+  });
+  return promise;
+}
 
+calculateSquare(1)
+  .then((value) => {
+    console.log(value);
+    return calculateSquare(2);
+  })
+  .then(
+    (value) => {
+      throw new Error("some random error");
+
+      //   console.log(value);
+    },
+    (reason) => {
+      console.log("error happened: " + reason.message);
+    }
+  );
