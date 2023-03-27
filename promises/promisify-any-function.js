fdf3c5d6d1813acf3245d081419151a970ed20dc
Raj Jain <raj.jain@credera.co.uk>
2023-03-27 11:29:07 +0100

    promisify any function

--- promises/promisify-any-function.js
+++ promises/promisify-any-function.js
@@ -1,5 +1,24 @@
+// function capitalize(text) {
+//   return text[0].toUpperCase() + text.substr(1);
+// }
+
+// console.log(capitalize("hi"));
+
 function capitalize(text) {
-  return text[0].toUpperCase() + text.substr(1);
+  return new Promise(function (resolve, reject) {
+    if (typeof text !== "string") {
+      reject(new Error("Argument of type string is expected"));
+    }
+    const result = text[0].toUpperCase() + text.substr(1);
+    resolve(result);
+  });
 }
 
-console.log(capitalize("hi"));
+capitalize("Random").then(
+  (value) => {
+    console.log(value);
+  },
+  (error) => {
+    console.log(error.message);
+  }
+);
