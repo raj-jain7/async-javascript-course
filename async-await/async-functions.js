9f0dcf5ebc973ceddeb818a562aa0bc6e1b3c583
Raj Jain <raj.jain@credera.co.uk>
2023-03-29 19:13:26 +0100

    await

--- async-await/async-functions.js
+++ async-await/async-functions.js
@@ -1,1 +1,23 @@
+// Async functions always return a promise.
+// If such a value is not a promise then JavaScript would wrap it in a promise
 
+// Javascript will automatically create a promise around that value
+// The promise will be resolved with that value
+async function f() {
+  return "Hello World";
+}
+f().then((value) => {
+  console.log(value);
+});
+
+async function newFunc() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Hi");
+    }, 1000);
+  });
+}
+
+newFunc().then((value) => {
+  console.log(value);
+});
---
+++ async-await/await.js
@@ -0,0 +1,36 @@
+// Await is used to wait for a promise
+// Waits until the promise is settled. Does not matter if it is resolved or rejected
+function getSpecificNumber() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(42), 4000);
+  });
+}
+
+function getAnotherSpecificNumber() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => resolve(44), 0);
+  });
+}
+
+// Option 1
+async function f() {
+  const specificNumber = await getSpecificNumber();
+  const anotherSpecificNumber = await getAnotherSpecificNumber();
+  console.log(specificNumber);
+  console.log(anotherSpecificNumber);
+}
+
+f();
+// Both values are loaded at the same time
+
+// Option 2
+function func() {
+  getSpecificNumber().then((value) => {
+    console.log(value);
+  });
+  getAnotherSpecificNumber().then((value) => {
+    console.log(value);
+  });
+}
+
+func();
